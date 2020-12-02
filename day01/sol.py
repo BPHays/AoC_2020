0aa0bfd2c30176ac67ac015b650728f7c2397933
Brian Hays <hays1@purdue.edu>
2020-12-01 19:16:13 -0800

    postmature optimization

--- day01/sol.py
+++ day01/sol.py
@@ -3,10 +3,7 @@
 nums = {int(num) for num in open("input").readlines()}
 num_pairs = {x + y: x * y for x in nums for y in nums}
 
-for x in nums:
-    if (2020 - x) in nums:
-        print(x * (2020 - x))
-        break
+print(num_pairs[2020])
 
 for x, prod in num_pairs.items():
     if (2020 - x) in nums:
